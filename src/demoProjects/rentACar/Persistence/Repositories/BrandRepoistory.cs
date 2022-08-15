d97777951d03d3d6eec5ced6b7fc97583ba28b5b
oğuzhan göley <oguzhanngoley@gmail.com>
2022-08-15 23:06:09 +0300

    initial

---
+++ src/demoProjects/rentACar/Persistence/Repositories/BrandRepoistory.cs
@@ -0,0 +1,20 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class BrandRepoistory : EfRepositoryBase<Brand, BaseDbContext>,IBrandRepository
+    {
+        public BrandRepoistory(BaseDbContext context) : base(context)
+        {
+
+        }
+    }
+}
